f3ba3a05e71de8b74aab73a0a6ba827c5128714d
Christina Tang <christinatangerine@hotmail.com>
2019-09-18 19:40:45 -0700

    created click events for gems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
 
         //** Returns a random integer between min and max ---> Math.floor(Math.random() * (max - min + 1)) + min;
 
-        var targetScore = Math.floor(Math.rand() * 102)+19;
-        var gemOne = Math.floor(Math.rand()*12)+1;
-        var gemTwo = Math.floor(Math.rand()*12)+1;
-        var gemThree = Math.floor(Math.rand()*12)+1;
-        var gemFour = Math.floor(Math.rand()*12)+1;
+        var targetScore = Math.floor(Math.random() * 102)+19;
+        var gemOne = Math.floor(Math.random()*12)+1;
+        var gemTwo = Math.floor(Math.random()*12)+1;
+        var gemThree = Math.floor(Math.random()*12)+1;
+        var gemFour = Math.floor(Math.random()*12)+1;
         var yourScore = 0;
         var winCount = 0;
         var lossCount = 0;
@@ -22,14 +22,18 @@ $(document).ready(function() {
         $("#target-score").text(targetScore);
         $("#loss-count").text(lossCount);
         $("#win-count").text(winCount);
+        $("gem-one").text(gemOne);
+        $("gem-two").text(gemTwo);
+        $("gem-three").text(gemThree);
+        $("gem-four").text(gemFour);
 
         //Reset function, setting your score back to zero after winning or losing.
         function reset () {
-        targetScore = Math.floor(Math.rand() * 102)+19;
-        gemOne = Math.floor(Math.rand()*12)+1;
-        gemTwo = Math.floor(Math.rand()*12)+1;
-        gemThree = Math.floor(Math.rand()*12)+1;
-        gemFour = Math.floor(Math.rand()*12)+1;
+        targetScore = Math.floor(Math.random() * 102)+19;
+        gemOne = Math.floor(Math.random()*12)+1;
+        gemTwo = Math.floor(Math.random()*12)+1;
+        gemThree = Math.floor(Math.random()*12)+1;
+        gemFour = Math.floor(Math.random()*12)+1;
         score = 0;
         }
 
@@ -51,9 +55,23 @@ $(document).ready(function() {
 
         //Jquery click events
 
-        $("#gem-one").on("click") {
+        $("#gem-one").on("click"); {
             yourScore = gemOne + score;
         }
+
+        $("#gem-two").on("click"); {
+            yourScore = gemTwo + score;
+        }
+
+        $("#gem-three").on("click"); {
+            yourScore = gemThree + score;
+        }
+
+        $("#gem-four").on("click"); {
+            yourScore = gemFour + score;
+        }
+
+        
     
     
         
